e19f06679a64991f52cfd952b23db3ab51af448c
eveporcello <eve@moonhighway.com>
2023-08-04 14:37:21 -0700

    Adds auth buttons and such

--- app/hotels/HotelBlock.js
+++ app/hotels/HotelBlock.js
@@ -13,6 +13,7 @@ export default function HotelBlock({
   const imageLoader = ({ src }) => {
     return `./hotels/${src}.jpeg`;
   };
+
   return (
     <div className="bg-white rounded-lg p-4 shadow">
       <h2 className="text-xl font-bold mb-2">{name}</h2>
--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
 import Header from "./Header";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import GitHubButtonServer from "./GitHubButtonServer";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.from("counter").select();
+
   return (
     <>
       <Header />
-      <p>{JSON.stringify(data)}</p>
+      <GitHubButtonServer />
+      {data.length ? <p>{data.length} days</p> : null}
       <div className="bg-hero-image bg-cover bg-center h-screen flex items-center justify-center">
         <div>
           <h1 className="text-7xl font-bold text-white mb-6 bg-slate-600">
